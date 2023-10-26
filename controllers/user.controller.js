dcf7c22737e0ff731f73651b5899f6acedc411a9
Bagas Udi Sahsangka <udibagas@gmail.com>
2023-10-26 21:40:43 +0700

    remove async

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,7 +3,7 @@ const ApiController = require("./api.controller")(User);
 const isAdmin = require("../middlewares/admin.middleware");
 
 class UserController extends ApiController {
-  static async index(req, res, next) {
+  static index(req, res, next) {
     isAdmin(req, res, (err) => {
       if (err) return next(err);
       super.index(req, res);
