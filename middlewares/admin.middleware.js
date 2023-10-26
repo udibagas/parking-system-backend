4a6eca3df3f0321de9bdc0548de0aae0050146ae
Bagas Udi Sahsangka <udibagas@gmail.com>
2023-10-26 22:11:02 +0700

    only get active user

--- middlewares/admin.middleware.js
+++ middlewares/admin.middleware.js
@@ -3,8 +3,10 @@ const { User } = require("../models");
 
 module.exports = async (req, res, next) => {
   try {
-    // const user = await User.findByPk(req.user.id, { where: { role: 1 } });
-    const user = await User.findOne({ where: { id: req.user.id, role: 1 } });
+    const user = await User.findOne({
+      where: { id: req.user.id, role: 1, status: true },
+    });
+
     if (!user) {
       throw new NotAuthorizedError("Anda tidak berhak mengakses halaman ini");
     }
